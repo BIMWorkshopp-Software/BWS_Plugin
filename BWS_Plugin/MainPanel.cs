dc264746bae02670f2284385fa8465210805606a
Юрий Чебелюк <chebelyuk_y@mail.ru>
2022-08-16 00:24:37 +0500

    Создана главная панель

---
+++ BWS_Plugin/MainPanel.cs
@@ -0,0 +1,26 @@
+﻿using VCRevitRibbonUtil;
+using Autodesk.Revit.UI;
+using BWS_Plugin.Properties;
+using BWS_Plugin;
+
+
+
+internal class MainPanel : IExternalApplication
+{
+    public Result OnStartup(UIControlledApplication a)
+    {
+        Ribbon.GetApplicationRibbon(a)
+            .Tab("BWS").Panel("Плагины")
+            .CreateButton<Test>("Info", "Info", b => 
+            b.SetLargeImage(Resources.BWS_Logo_32)
+            .SetSmallImage(Resources.BWS_Logo_16)
+            .SetLongDescription("Информация о плагинах Bim Workshopp")           
+            );
+
+        return Result.Succeeded;
+    }
+    public Result OnShutdown(UIControlledApplication a)
+    {
+        return Result.Succeeded;
+    }
+}
---
+++ BWS_Plugin/Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace BWS_Plugin
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Test : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var uiapp = commandData.Application;
+            var uidoc = uiapp.ActiveUIDocument;
+            var doc = uidoc.Document;
+
+            //Основной код плагина
+
+            return Result.Succeeded;
+        }
+
+    }
+}
