30c2cc773f37f732436041c83ddbee282562941f
Юрий Чебелюк <chebelyuk_y@mail.ru>
2022-10-02 15:47:39 +0500

    Добавлены классы с категориями, гуидами и помошниками

---
+++ BWS_Plugin/Helpers/AllUsedCategoryList.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+
+namespace BWS_Plugin
+{
+    internal class AllUsedCategoryList
+    {
+        public IEnumerable<BuiltInCategory> categoryList = new BuiltInCategory[] {
+            BuiltInCategory.OST_PipeAccessory, //Арматура трубопроводов 
+            BuiltInCategory.OST_MechanicalEquipment, //Оборудование 
+            BuiltInCategory.OST_PlumbingFixtures, //Сантехнические приборы 
+            BuiltInCategory.OST_PipeFitting, //Соединительные детали трубопроводов 
+            BuiltInCategory.OST_PipeInsulations, //Материалы изоляции труб 
+            BuiltInCategory.OST_DuctCurves, //Воздуховоды 
+            BuiltInCategory.OST_DuctFitting, //Соединительные детали воздуховодов 
+            BuiltInCategory.OST_DuctAccessory, //Арматура воздуховодов 
+            BuiltInCategory.OST_DuctTerminal, //Воздухораспределители 
+            BuiltInCategory.OST_CableTray, //Кабельные лотки 
+            BuiltInCategory.OST_Conduit, //Короба 
+            BuiltInCategory.OST_CableTrayFitting, //Соединительные детали кабельных лотков 
+            BuiltInCategory.OST_ConduitFitting, //Соединительные детали коробов 
+            BuiltInCategory.OST_FireAlarmDevices, //Пожарная сигнализация 
+            BuiltInCategory.OST_DataDevices, //Датчики 
+            BuiltInCategory.OST_ElectricalEquipment, //Электрооборудование 
+            BuiltInCategory.OST_ElectricalFixtures, //Электрические приборы 
+            BuiltInCategory.OST_LightingFixtures,  //Осветительные приборы 
+            BuiltInCategory.OST_DuctInsulations, //Материалы изоляции воздуховодов 
+            BuiltInCategory.OST_LinksAnalytical,
+            BuiltInCategory.OST_Casework,
+            BuiltInCategory.OST_Ceilings,
+            BuiltInCategory.OST_Columns,
+            BuiltInCategory.OST_CommunicationDevices,
+            BuiltInCategory.OST_CurtainWallPanels,
+            BuiltInCategory.OST_CurtaSystem,
+            BuiltInCategory.OST_CurtainWallMullions,
+            BuiltInCategory.OST_DetailComponents,
+            BuiltInCategory.OST_Doors,
+            BuiltInCategory.OST_ElectricalCircuit,
+            BuiltInCategory.OST_Entourage,
+            BuiltInCategory.OST_FlexDuctCurves,
+            BuiltInCategory.OST_FlexPipeCurves,
+            BuiltInCategory.OST_Floors,
+            BuiltInCategory.OST_Furniture,
+            BuiltInCategory.OST_FurnitureSystems,
+            BuiltInCategory.OST_GenericModel,
+            BuiltInCategory.OST_LightingDevices,
+            BuiltInCategory.OST_Mass,
+            BuiltInCategory.OST_NurseCallDevices,
+            BuiltInCategory.OST_Parking,
+            BuiltInCategory.OST_Parts,
+            BuiltInCategory.OST_PipeCurves,
+            BuiltInCategory.OST_StairsRailing,
+            BuiltInCategory.OST_Ramps,
+            BuiltInCategory.OST_RebarShape,
+            BuiltInCategory.OST_Roofs,
+            BuiltInCategory.OST_SecurityDevices,
+            BuiltInCategory.OST_ShaftOpening,
+            BuiltInCategory.OST_Site,
+            BuiltInCategory.OST_MEPSpaces,
+            BuiltInCategory.OST_SpecialityEquipment,
+            BuiltInCategory.OST_Sprinklers,
+            BuiltInCategory.OST_Stairs,
+            BuiltInCategory.OST_AreaRein,
+            BuiltInCategory.OST_StructuralFramingSystem,
+            BuiltInCategory.OST_StructuralColumns,
+            BuiltInCategory.OST_StructConnections,
+            BuiltInCategory.OST_FabricAreas,
+            BuiltInCategory.OST_FabricReinforcement,
+            BuiltInCategory.OST_StructuralFoundation,
+            BuiltInCategory.OST_StructuralFraming,
+            BuiltInCategory.OST_PathRein,
+            BuiltInCategory.OST_Rebar,
+            BuiltInCategory.OST_StructuralStiffener,
+            BuiltInCategory.OST_StructuralTruss,
+            BuiltInCategory.OST_SwitchSystem,
+            BuiltInCategory.OST_TelephoneDevices,
+            BuiltInCategory.OST_Walls,
+            BuiltInCategory.OST_Windows,
+            BuiltInCategory.OST_Wire
+        };
+    }
+}
---
+++ BWS_Plugin/Helpers/Helpers.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+
+namespace BWS_Plugin
+{
+    internal class Helpers
+    {
+        public double feetToMillimeters(double feet)
+        {
+            double a = feet;
+            return a * 304.8;
+        }
+
+        public double millimetersToFeet(double millimetrs)
+        {
+            double a = millimetrs;
+            return (a / 304.8);
+        }
+        public IList<Element> AllElementsOfCategory(Document document, BuiltInCategory category)
+        {
+            var collector = new FilteredElementCollector(document);
+            return collector.OfCategory(category).WhereElementIsNotElementType().ToElements();
+        }
+
+        public IList<Element> AllElementsInActiveViewOfCategory(Document document, BuiltInCategory category)
+        {
+            var collector = new FilteredElementCollector(document, document.ActiveView.Id);
+            return collector.OfCategory(category).WhereElementIsNotElementType().ToElements();
+        }
+
+        public IList<Element> AllElementsInActiveView(Document document)
+        {
+            var collector = new FilteredElementCollector(document, document.ActiveView.Id);
+            return collector.WhereElementIsNotElementType().ToElements();
+        }
+
+        public IList<Element> AllElementsInProject(Document document)
+        {
+            IList<Element> allElements = new List<Element>();
+            AllUsedCategoryList categories = new AllUsedCategoryList();
+            foreach (var category in categories.categoryList)
+            {
+                try
+                {
+                    var elem = AllElementsOfCategory(document, category);
+                    foreach (var element in elem)
+                    {
+                        allElements.Add(element);
+                    }
+                }
+                catch { };
+            }
+            return allElements;
+        }
+    }
+}
